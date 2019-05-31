58f7907bcc36e4bb8381fa4af4b8cf76222d30a7
18834825727 <815130927@qq.com>
2019-05-31 18:04:32 +0800

    cache commit

--- src/main/java/com/xiao/blog/BlogApplication.java
+++ src/main/java/com/xiao/blog/BlogApplication.java
@@ -3,11 +3,13 @@ package com.xiao.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 //@EnableTransactionManagement
 @MapperScan("com.xiao.blog.*.mapper")
+@EnableCaching
 @SpringBootApplication
 public class BlogApplication {
 
